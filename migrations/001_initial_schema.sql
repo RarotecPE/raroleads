0ca76d63130c80209ee6a3037a51ebc41e7bc7c5
Rodrigo Souza Barbosa <82851436+rodsouzab@users.noreply.github.com>
2026-08-25 17:08:32 -0300

    fix: ajustar erros de migration

--- migrations/001_initial_schema.sql
+++ migrations/001_initial_schema.sql
@@ -1,96 +1,97 @@
 CREATE TABLE "clients" (
-  "id" intenger PRIMARY KEY,
+  "id" integer PRIMARY KEY,
   "cnpj" varchar NOT NULL,
   "name" varchar NOT NULL,
   "uf" varchar(2),
   "type" varchar,
-  "codigo_ibge" intenger,
+  "codigo_ibge" integer,
   "population" integer,
   "created_at" timestamp,
   "state" varchar
 );
 
 CREATE TABLE "modules" (
-  "id" intenger PRIMARY KEY,
+  "id" integer PRIMARY KEY,
   "name" varchar NOT NULL
 );
 
 CREATE TABLE "bases" (
-  "id" intenger PRIMARY KEY,
+  "id" integer PRIMARY KEY,
   "name" varchar NOT NULL
 );
 
 CREATE TABLE "clients_bases" (
-  "id" intenger NOT NULL,
-  "clients_id" intenger NOT NULL,
-  "bases_id" intenger NOT NULL,
+  "id" integer PRIMARY KEY,
+  "clients_id" integer NOT NULL,
+  "bases_id" integer NOT NULL,
   "cnpj" varchar NOT NULL,
   "state" varchar,
   "observation" text,
-  "date" datetime
+  "date" timestamp
 );
 
 CREATE TABLE "client_bases_history" (
-  "id" intenger NOT NULL,
-  "clients_bases_id" intenger NOT NULL,
+  "id" integer PRIMARY KEY,
+  "clients_bases_id" integer NOT NULL,
   "state" varchar,
   "observation" text,
-  "date" datetime
+  "date" timestamp
 );
 
 CREATE TABLE "clients_bases_module" (
-  "id" intenger NOT NULL,
-  "clients_bases_id" intenger NOT NULL,
-  "modules_id" intenger NOT NULL,
+  "id" integer PRIMARY KEY,
+  "clients_bases_id" integer NOT NULL,
+  "modules_id" integer NOT NULL,
   "responsible" varchar,
   "state" varchar,
   "has_contract" bool,
-  "module_contract_id" intenger,
+  "module_contract_id" integer,
   "observation" text,
-  "date" datetime
+  "date" timestamp
 );
 
 CREATE TABLE "clients_bases_module_history" (
-  "id" intenger NOT NULL,
-  "clients_bases_module_id" intenger NOT NULL,
+  "id" integer PRIMARY KEY,
+  "clients_bases_module_id" integer NOT NULL,
   "responsible" varchar,
   "state" varchar,
   "has_contract" bool,
-  "module_contract_id" intenger,
+  "module_contract_id" integer,
   "document_id" integer,
   "observation" text,
   "user" varchar,
-  "date" datetime
+  "date" timestamp
 );
 
 CREATE TABLE "module_contract" (
-  "id" intenger,
-  "clients_bases_module_id" intenger,
-  "number" intenger,
-  "initial_date" datetime,
-  "final_date" datetime,
-  "sign_date" datetime,
+  "id" integer,
+  "clients_bases_module_id" integer UNIQUE,
+  "number" integer,
+  "initial_date" timestamp,
+  "final_date" timestamp,
+  "sign_date" timestamp,
   "state" varchar,
   "observation" text,
   "has_amendment" bool,
-  "created_at" datetime,
-  PRIMARY KEY ("id", "clients_bases_module_id")
+  "created_at" timestamp,
+  PRIMARY KEY ("id", "clients_bases_module_id"),
+  UNIQUE ("id")
 );
 
 CREATE TABLE "contract_amendment" (
-  "id" intenger PRIMARY KEY,
-  "module_contract_id" intenger NOT NULL,
+  "id" integer PRIMARY KEY,
+  "module_contract_id" integer NOT NULL,
   "type" varchar NOT NULL,
-  "date" datetime
+  "date" timestamp
 );
 
 CREATE TABLE "documents" (
-  "id" intenger PRIMARY KEY,
-  "clients_bases_module" intenger NOT NULL,
+  "id" integer PRIMARY KEY,
+  "clients_bases_module" integer NOT NULL,
   "type" varchar,
   "name" varchar,
   "observation" varchar,
-  "created_at" datetime
+  "created_at" timestamp
 );
 
 ALTER TABLE "clients_bases" ADD FOREIGN KEY ("clients_id") REFERENCES "clients" ("id") DEFERRABLE INITIALLY IMMEDIATE;
@@ -98,61 +99,57 @@ ALTER TABLE "clients_bases" ADD FOREIGN KEY ("clients_id") REFERENCES "clients" 
 ALTER TABLE "clients_bases" ADD FOREIGN KEY ("bases_id") REFERENCES "bases" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
 CREATE TABLE "clients_bases_module_clients_bases" (
-  "clients_bases_module_clients_bases_id" intenger,
-  "clients_bases_id" intenger,
+  "clients_bases_module_clients_bases_id" integer,
+  "clients_bases_id" integer,
   PRIMARY KEY ("clients_bases_module_clients_bases_id", "clients_bases_id")
 );
 
-ALTER TABLE "clients_bases_module_clients_bases" ADD FOREIGN KEY ("clients_bases_module_clients_bases_id") REFERENCES "clients_bases_module" ("clients_bases_id") DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE "clients_bases_module_clients_bases" ADD FOREIGN KEY ("clients_bases_module_clients_bases_id") REFERENCES "clients_bases_module" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE "clients_bases_module_clients_bases" ADD FOREIGN KEY ("clients_bases_id") REFERENCES "clients_bases" ("id") DEFERRABLE INITIALLY IMMEDIATE;
-
 
 ALTER TABLE "clients_bases_module" ADD FOREIGN KEY ("modules_id") REFERENCES "modules" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
 CREATE TABLE "client_bases_history_clients_bases" (
-  "client_bases_history_clients_bases_id" intenger,
-  "clients_bases_id" intenger,
+  "client_bases_history_clients_bases_id" integer,
+  "clients_bases_id" integer,
   PRIMARY KEY ("client_bases_history_clients_bases_id", "clients_bases_id")
 );
 
-ALTER TABLE "client_bases_history_clients_bases" ADD FOREIGN KEY ("client_bases_history_clients_bases_id") REFERENCES "client_bases_history" ("clients_bases_id") DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE "client_bases_history_clients_bases" ADD FOREIGN KEY ("client_bases_history_clients_bases_id") REFERENCES "client_bases_history" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE "client_bases_history_clients_bases" ADD FOREIGN KEY ("clients_bases_id") REFERENCES "clients_bases" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
-
 CREATE TABLE "clients_bases_module_history_clients_bases_module" (
-  "clients_bases_module_history_clients_bases_module_id" intenger,
-  "clients_bases_module_id" intenger,
+  "clients_bases_module_history_clients_bases_module_id" integer,
+  "clients_bases_module_id" integer,
   PRIMARY KEY ("clients_bases_module_history_clients_bases_module_id", "clients_bases_module_id")
 );
 
-ALTER TABLE "clients_bases_module_history_clients_bases_module" ADD FOREIGN KEY ("clients_bases_module_history_clients_bases_module_id") REFERENCES "clients_bases_module_history" ("clients_bases_module_id") DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE "clients_bases_module_history_clients_bases_module" ADD FOREIGN KEY ("clients_bases_module_history_clients_bases_module_id") REFERENCES "clients_bases_module_history" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE "clients_bases_module_history_clients_bases_module" ADD FOREIGN KEY ("clients_bases_module_id") REFERENCES "clients_bases_module" ("id") DEFERRABLE INITIALLY IMMEDIATE;
-
 
 ALTER TABLE "clients_bases_module" ADD FOREIGN KEY ("id") REFERENCES "module_contract" ("clients_bases_module_id") DEFERRABLE INITIALLY IMMEDIATE;
 
 CREATE TABLE "documents_clients_bases_module" (
-  "documents_clients_bases_module" intenger,
-  "clients_bases_module_id" intenger,
+  "documents_clients_bases_module" integer,
+  "clients_bases_module_id" integer,
   PRIMARY KEY ("documents_clients_bases_module", "clients_bases_module_id")
 );
 
-ALTER TABLE "documents_clients_bases_module" ADD FOREIGN KEY ("documents_clients_bases_module") REFERENCES "documents" ("clients_bases_module") DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE "documents_clients_bases_module" ADD FOREIGN KEY ("documents_clients_bases_module") REFERENCES "documents" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE "documents_clients_bases_module" ADD FOREIGN KEY ("clients_bases_module_id") REFERENCES "clients_bases_module" ("id") DEFERRABLE INITIALLY IMMEDIATE;
-
 
 ALTER TABLE "clients_bases_module" ADD FOREIGN KEY ("module_contract_id") REFERENCES "module_contract" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
 CREATE TABLE "contract_amendment_module_contract" (
-  "contract_amendment_module_contract_id" intenger,
-  "module_contract_id" intenger,
+  "contract_amendment_module_contract_id" integer,
+  "module_contract_id" integer,
   PRIMARY KEY ("contract_amendment_module_contract_id", "module_contract_id")
 );
 
-ALTER TABLE "contract_amendment_module_contract" ADD FOREIGN KEY ("contract_amendment_module_contract_id") REFERENCES "contract_amendment" ("module_contract_id") DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE "contract_amendment_module_contract" ADD FOREIGN KEY ("contract_amendment_module_contract_id") REFERENCES "contract_amendment" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE "contract_amendment_module_contract" ADD FOREIGN KEY ("module_contract_id") REFERENCES "module_contract" ("id") DEFERRABLE INITIALLY IMMEDIATE;
